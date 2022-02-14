e339a10afdb0b0cf1ebbd915851d7c176c64c2d9
sudhir561 <sudhirme101@gmail.com>
2022-02-14 21:12:21 +0530

    store the token in memory

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from "./components/Preferences/Preferences";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Login from "./components/Login/Login";
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
+  //store the token in memory using the useState Hook.
+const [token,setToken]=useState();
+
+if(!token){
+  return <Login setToken={setToken}/>
+}
   return (
  <div className="wrapper">
 
@@ -17,7 +24,7 @@ function App() {
  <Route path="/preferences" element={<Preferences/>}></Route>
   </Routes>
   </BrowserRouter>
-  <Login/>
+ 
   </div>
   );
 }
